216757966aacb0c9a38ee4b5e09f468c91f89481
Vishwas Hk <Vishwas.HK@mea.ey.net>
2017-09-18 13:01:48 +0530

    Thermometer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation, Renderer2, ElementRef, AfterViewInit, ViewChild} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,32 +6,8 @@ import {Component, ViewEncapsulation, Renderer2, ElementRef, AfterViewInit, View
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
   title = 'app';
-  @ViewChild('rangeSlider') rangeSlider: any;
-  sideSlideConfig = {
-    connect: true,
-    range: {
-      min: 0,
-      max: 99
-    },
-    step: 1,
-    direction:'rtl',
-    orientation: 'vertical',
-    behaviour: 'tap-drag'
-  };
-  someRange= [0, 25];
-  constructor (private renderer: Renderer2) {
-  }
-  
-  ngAfterViewInit(){
-    this.renderer.addClass(this.rangeSlider.el.nativeElement.firstElementChild, 'meter-active');
-  }
-  captureChange($event) {
-    // console.log($event);
-  }
-
-  startRange($event, handle, encode) {
-    // this.renderer.addClass(handle.el.nativeElement.firstElementChild, 'meter-active');
+  constructor () {
   }
 }
--- src/app/my-slider/my-slider.component.ts
+++ src/app/my-slider/my-slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgModule} from "@angular/core";
+import {Component, ViewEncapsulation, Renderer2, ElementRef, AfterViewInit, ViewChild} from '@angular/core';
 /**
  * Created by jason on 16/09/17.
  */
@@ -8,7 +8,27 @@ import {Component, NgModule} from "@angular/core";
   templateUrl: './my-slider.component.html',
   styleUrls: ['./my-slider.component.css']
 })
-export class MySliderComponent {
-  constructor (){}
-
+export class MySliderComponent implements AfterViewInit {
+  @ViewChild('rangeSlider') rangeSlider: any;
+  sideSlideConfig = {
+    connect: true,
+    range: {
+      min: 0,
+      max: 99
+    },
+    step: 1,
+    direction:'rtl',
+    orientation: 'vertical',
+    behaviour: 'tap-drag'
+  };
+  someRange= [0, 25];
+  constructor (private renderer: Renderer2) {
+  }
+  
+  ngAfterViewInit(){
+    this.renderer.addClass(this.rangeSlider.el.nativeElement.firstElementChild, 'meter-active');
+  }
+  captureChange($event) {
+    console.log($event);
+  }
 }
